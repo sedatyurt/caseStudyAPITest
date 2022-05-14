51ae5242a934115feac7c482f48f7713e0bd43dd
syurt <sedatyurt34@gmail.com>
2022-05-14 22:51:20 +0300

    Added write responses to file

--- src/main/java/utils/FileHelper.java
+++ src/main/java/utils/FileHelper.java
@@ -1,8 +1,19 @@
 package utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class FileHelper {
 
-    public static void writeResponseToFile(){
-
+    public static void writeResponseToFile(String str) throws IOException {
+        String path = "responses/" + LocalDateTime.now() + "_RESPONSE.json";
+        File file = new File(path);
+        FileWriter fileWriter = new FileWriter(file, false);
+        BufferedWriter bWriter = new BufferedWriter(fileWriter);
+        bWriter.write(str);
+        bWriter.close();
     }
 }
--- src/test/java/apitest/CaseStudyTests.java
+++ src/test/java/apitest/CaseStudyTests.java
@@ -3,6 +3,8 @@ package apitest;
 import client.SendInvoiceClient;
 import client.TokenClient;
 import client.ViewInvoiceClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.response.Response;
 import model.request.Invoice;
 import model.request.User;
@@ -11,7 +13,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.FileHelper;
 
+import java.io.IOException;
+
 public class CaseStudyTests {
+
+    ObjectMapper obj = new ObjectMapper();
+
 
     @Test
     public void whenUserPostTokenServiceShouldRetrieveToken()  {
@@ -25,24 +32,26 @@ public class CaseStudyTests {
     }
 
     @Test
-    public void whenUserGetViewInvoiceServiceShouldRetrieveInvoiceLink() {
+    public void whenUserGetViewInvoiceServiceShouldRetrieveInvoiceLink() throws IOException {
 
         ViewInvoiceClient viewInvoiceClient = new ViewInvoiceClient();
         ViewInvoiceResponse viewInvoiceResponse = viewInvoiceClient.createViewInvoiceRequest();
         Assert.assertEquals(viewInvoiceResponse.getInvoiceLink(),"http://abc.com/invoice.pdf");
         Assert.assertEquals(viewInvoiceResponse.getResult().isSuccess(),true);
+        FileHelper.writeResponseToFile(obj.writeValueAsString(viewInvoiceResponse));
+
 
     }
 
     @Test
-    public void whenUserPostSendInvoiceServiceShouldBeSuccess() {
+    public void whenUserPostSendInvoiceServiceShouldBeSuccess() throws IOException {
 
         TokenClient tokenClient = new TokenClient();
         Response tokenResponse = tokenClient.createTokenRequest(new User("sedatyurt", "Test12345"));
         SendInvoiceClient sendInvoiceClient = new SendInvoiceClient();
         Response response = sendInvoiceClient.createSendInvoiceRequest(tokenResponse.path("token"), new Invoice("barcode"));
         Assert.assertEquals(response.statusCode(),200);
-        FileHelper.writeResponseToFile();
+        FileHelper.writeResponseToFile(response.body().prettyPrint());
 
 
     }
